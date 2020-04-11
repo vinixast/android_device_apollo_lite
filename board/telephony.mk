66fbc3bceaf278ef6fe36c8f794b4649f68c1613
vinicius <vinny.ast95@gmail.com>
2020-04-10 23:56:06 -0300

    Remove 2 sim product property overrides

--- board/telephony.mk
+++ board/telephony.mk
@@ -2,5 +2,5 @@
 BOARD_RIL_CLASS := ../../../device/vernee/apollo_lite/ril
 BOARD_CONNECTIVITY_MODULE := conn_soc
 
-SIM_COUNT := 2
-PRODUCT_PROPERTY_OVERRIDES += ro.telephony.sim.count=$(SIM_COUNT)
+#SIM_COUNT := 2
+#PRODUCT_PROPERTY_OVERRIDES += ro.telephony.sim.count=$(SIM_COUNT)
