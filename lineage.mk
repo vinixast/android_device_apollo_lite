56fd634cfcb7cdd5631144850976a8c0b8c7740b
vinixast <63014441+vinixast@users.noreply.github.com>
2020-04-17 04:11:03 -0300

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -6,19 +6,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from hardware-specific part of the product configuration
-$(call inherit-product, device/vernee/apollo_lite/device.mk)
+$(call inherit-product, device/vernee/apollo_x/device.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 
 # Release name
-PRODUCT_RELEASE_NAME := apollo_lite
+PRODUCT_RELEASE_NAME := apollo_x
 
-PRODUCT_DEVICE := apollo_lite
-PRODUCT_NAME := lineage_apollo_lite
+PRODUCT_DEVICE := apollo_x
+PRODUCT_NAME := lineage_apollo_x
 PRODUCT_BRAND := Vernee
-PRODUCT_MODEL := Apollo_Lite
+PRODUCT_MODEL := Apollo_x
 PRODUCT_MANUFACTURER := Vernee
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
